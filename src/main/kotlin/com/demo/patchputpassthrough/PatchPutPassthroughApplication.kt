2a4f14f6c48fd17ffca5bc10933e6d19fa648552
Vineeth Venudasan <vineethv@thoughtworks.com>
2024-05-20 06:28:13 +0200

    Initial commit

---
+++ src/main/kotlin/com/demo/patchputpassthrough/PatchPutPassthroughApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.patchputpassthrough
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PatchPutPassthroughApplication
+
+fun main(args: Array<String>) {
+    runApplication<PatchPutPassthroughApplication>(*args)
+}
---
+++ src/test/kotlin/com/demo/patchputpassthrough/PatchPutPassthroughApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.demo.patchputpassthrough
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PatchPutPassthroughApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
